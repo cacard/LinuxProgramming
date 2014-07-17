b866a53746519d4ed592014f92d6dbb36a176afb
cacard <cacard@126.com>
2014-07-17 16:35:37 +0800

    -

---
+++ 130.ipc.posix.sem.c
@@ -0,0 +1,18 @@
+//
+//	Posix 有名信号量
+//
+//	实现
+//	- 基于文件，具体实现也有基于内存的
+//		- 基于FIFO
+//		- 基于mmap
+//
+//	相关函数
+//	sem_oepn
+//	.wait
+//	.trywait
+//	.post
+//	.getvalue
+//	.close
+//	.unlink
+//
+//
